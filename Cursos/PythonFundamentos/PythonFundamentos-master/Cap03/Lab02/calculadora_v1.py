0910553b5c50a4db3183f15c7809d4f5a57c0308
Gabriel <34110789+gsscalione-fcav@users.noreply.github.com>
2021-05-05 11:38:42 -0300

    desenvolvimetno da calculadora

--- Cursos/PythonFundamentos/PythonFundamentos-master/Cap03/Lab02/calculadora_v1.py
+++ Cursos/PythonFundamentos/PythonFundamentos-master/Cap03/Lab02/calculadora_v1.py
@@ -7,3 +7,43 @@
 print("\n******************* Python Calculator *******************")
 
 
+print("\nSelecione o número da operação desejada: ")
+
+print("\n1 - Soma")
+soma = lambda x,y: x + y
+
+print("2 - Subtração")
+sub = lambda x,y: x - y
+
+print("3 - Multiplicação")
+mult = lambda x,y: x * y
+
+print("4 - Divisão")
+div = lambda x,y: x / y
+
+operador = int(input("\nDigite a opção desajada: "))
+
+if operador > 4 or operador < 1:
+
+    print("\nOpção Inválida")
+
+else:
+    num1 = float(input("\nDigite o primeiro número: "))
+
+    num2 = float(input("Digite o segundo número: "))
+
+    if operador == 1:
+        
+        print(str(num1) + " + " + str(num2) + " = ", soma(num1, num2) )
+
+    elif operador == 2:
+
+        print(str(num1) + " - " + str(num2) + " = ", sub(num1, num2) )
+
+    elif operador == 3:
+
+        print(str(num1) + " * " + str(num2) + " = ", mult(num1, num2) )
+
+    elif operador == 4:
+
+        print(str(num1) + " / " + str(num2) + " = ", div(num1, num2) )
