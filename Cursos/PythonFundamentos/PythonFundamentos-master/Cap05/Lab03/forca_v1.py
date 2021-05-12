824de8731caa11c075cc20e86bf81af0d5987adb
Gabriel <34110789+gsscalione-fcav@users.noreply.github.com>
2021-05-12 11:41:17 -0300

    up

--- Cursos/PythonFundamentos/PythonFundamentos-master/Cap05/Lab03/forca_v1.py
+++ Cursos/PythonFundamentos/PythonFundamentos-master/Cap05/Lab03/forca_v1.py
@@ -70,24 +70,24 @@ O   |
 class Hangman:
 
 	# Método Construtor
-	def __init__(self, word):
-		self.word = word
-		self.missed_letters = []
-		self.guessed_letters = []
+	def __init__(self, palavra):
+		self.palavra = palavra
+		self.letras_erradas = []
+		self.letras_certas = []
 		
 	# Método para adivinhar a letra
-	def guess(self, letter):
-		if letter in self.word and letter not in self.guessed_letters:
-			self.guessed_letters.append(letter)
-		elif letter not in self.word and letter not in self.missed_letters:
-			self.missed_letters.append(letter)
+	def guess(self, letra):
+		if letra in self.palavra and letra not in self.letras_certas:
+			self.letras_certas.append(letra)
+		elif letra not in self.palavra and letra not in self.letras_erradas:
+			self.letras_erradas.append(letra)
 		else:
 			return False
 		return True
 		
 	# Método para verificar se o jogo terminou
 	def hangman_over(self):
-		return self.hangman_won() or (len(self.missed_letters) == 6)
+		return self.hangman_won() or (len(self.letras_erradas) == 6)
 		
 	# Método para verificar se o jogador venceu
 	def hangman_won(self):
@@ -98,24 +98,24 @@ class Hangman:
 	# Método para não mostrar a letra no board
 	def hide_word(self):
 		rtn = ''
-		for letter in self.word:
-			if letter not in self.guessed_letters:
+		for letra in self.palavra:
+			if letra not in self.letras_certas:
 				rtn += '_'
 			else:
-				rtn += letter
+				rtn += letra
 		return rtn
 		
 	# Método para checar o status do game e imprimir o board na tela
 	def print_game_status(self):
-		print (board[len(self.missed_letters)])
+		print (board[len(self.letras_erradas)])
 		print ('\nPalavra: ' + self.hide_word())
 		print ('\nLetras erradas: ',) 
-		for letter in self.missed_letters:
-			print (letter,) 
+		for letra in self.letras_erradas:
+			print (letra,) 
 		print ()
 		print ('Letras corretas: ',)
-		for letter in self.guessed_letters:
-			print (letter,)
+		for letra in self.letras_certas:
+			print (letra,)
 		print ()
 
 # Método para ler uma palavra de forma aleatória do banco de palavras
